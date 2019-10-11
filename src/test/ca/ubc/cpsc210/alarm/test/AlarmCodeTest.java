d9502e2eb996c40147cb57cfb422440a8de6c38e
Norm Hutchinson <norm@cs.ubc.ca>
2019-10-11 15:10:21 -0700

    Delete obsolete JUnit 5 libs and fix configuration.

--- src/test/ca/ubc/cpsc210/alarm/test/AlarmCodeTest.java
+++ src/test/ca/ubc/cpsc210/alarm/test/AlarmCodeTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
 /**
  * Unit tests for the AlarmCode class
  */
